c40df4b434455a0ad56be63f707e439055fc5b10
VJvishwa01 <vishuwadharsh21@gmail.com>
2024-06-12 12:36:57 +0530

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,95 +1,53 @@
 import random
-print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
-      + "Rock vs Paper - Paper wins \n"
-      + "Rock vs Scissors - Rock wins \n"
-      + "Paper vs Scissors - Scissor wins \n")
 
-while True:
+def get_computer_choice():
+    choices = ['rock', 'paper', 'scissors']
+    return random.choice(choices)
 
-    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+def determine_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "tie"
+    elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+         (user_choice == 'scissors' and computer_choice == 'paper') or \
+         (user_choice == 'paper' and computer_choice == 'rock'):
+        return "user"
+    else:
+        return "computer"
 
-    # take the input from user
+def display_result(user_choice, computer_choice, winner):
+    print(f"\nYou chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+    if winner == "tie":
+        print("It's a tie!")
+    elif winner == "user":
+        print("You win!")
+    else:
+        print("Computer wins!")
 
-    choice = int(input("Enter your choice :"))
+def main():
+    user_score = 0
+    computer_score = 0
 
-    # OR is the short-circuit operator
-    # if any one of the condition is true
-    # then it return True value
+    while True:
+        user_choice = input("Choose rock, paper, or scissors (or 'quit' to stop playing): ").lower()
+        if user_choice == 'quit':
+            break
+        if user_choice not in ['rock', 'paper', 'scissors']:
+            print("Invalid choice. Please choose rock, paper, or scissors.")
+            continue
 
-    # looping until user enter invalid input
-    while choice > 3 or choice < 1:
-        choice = int(input('Enter a valid choice please '))
+        computer_choice = get_computer_choice()
+        winner = determine_winner(user_choice, computer_choice)
+        display_result(user_choice, computer_choice, winner)
 
-        # initialize value of choice_name variable
-    # corresponding to the choice value
-    if choice == 1:
-        choice_name = 'Rock'
-    elif choice == 2:
-        choice_name = 'Paper'
-    else:
-        choice_name = 'Scissors'
+        if winner == "user":
+            user_score += 1
+        elif winner == "computer":
+            computer_score += 1
 
-        # print user choice
-    print('User choice is \n', choice_name)
-    print('Now its Computers Turn....')
+        print(f"Score - You: {user_score} | Computer: {computer_score}")
 
-    # Computer chooses randomly any number
-    # among 1 , 2 and 3. Using randint method
-    # of random module
-    comp_choice = random.randint(1, 3)
+    print("Thanks for playing!")
 
-    # looping until comp_choice value
-    # is equal to the choice value
-    while comp_choice == choice:
-        comp_choice = random.randint(1, 3)
-
-     # initialize value of comp_choice_name
-    # variable corresponding to the choice value
-    if comp_choice == 1:
-        comp_choice_name = 'RocK'
-    elif comp_choice == 2:
-        comp_choice_name = 'Paper'
-    else:
-        comp_choice_name = 'Scissors'
-    print("Computer choice is \n", comp_choice_name)
-    print(choice_name, 'Vs', comp_choice_name)
-    # we need to check of a draw
-    if choice == comp_choice:
-        print('Its a Draw', end="")
-        result = "DRAW"
-    # condition for winning
-    if (choice == 1 and comp_choice == 2):
-        print('paper wins =>', end="")
-        result = 'Paper'
-    elif (choice == 2 and comp_choice == 1):
-        print('paper wins =>', end="")
-        result = 'Paper'
-
-    if (choice == 1 and comp_choice == 3):
-        print('Rock wins =>\n', end="")
-        result = 'Rock'
-    elif (choice == 3 and comp_choice == 1):
-        print('Rock wins =>\n', end="")
-        result = 'RocK'
-
-    if (choice == 2 and comp_choice == 3):
-        print('Scissors wins =>', end="")
-        result = 'Scissors'
-    elif (choice == 3 and comp_choice == 2):
-        print('Scissors wins =>', end="")
-        result = 'Rock'
-     # Printing either user or computer wins or draw
-    if result == 'DRAW':
-        print("<== Its a tie ==>")
-    if result == choice_name:
-        print("<== User wins ==>")
-    else:
-        print("<== Computer wins ==>")
-    print("Do you want to play again? (Y/N)")
-    # if user input n or N then condition is True
-    ans = input().lower()
-    if ans == 'n':
-        break
-# after coming out of the while loop
-# we print thanks for playing
-print("thanks for playing")
+if __name__ == "__main__":
+    main()
